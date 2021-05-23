962f591a588f918e1effa1c3b079180d20d2906e
KaiwenJon <kaiwenjon23@gmail.com>
2021-05-23 16:32:41 +0800

    add babel package.

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,12 +1,10 @@
-import mongo from './mongo';
+import mongo from './mongo.js';
 import express from 'express';
-import cors from 'cors';
 import routes from './routes';
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import scoreCardRouter from './scoreCard';
+import register from './register'
 
 const router = Router();
 
 router.use('/', scoreCardRouter);
+router.use('/', register)
 
 export default router;
