3dfc629bf226e22a009dc1fae0ec1904b8a36291
KaiwenJon <kaiwenjon23@gmail.com>
2021-06-18 20:50:14 +0800

    mongoDB set up done.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,7 +5,19 @@ const Schema = mongoose.Schema
 // Creating a schema, sort of like working with an ORM
 const UserSchema = new Schema({
   username:{type:String, required: true, unique: true},
-  password:{type:String, required: true}
+  password:{type:String, required: true},
+  todoList:[{
+    name: String,
+    priority: String,
+    separate: Number,
+    needtime: Number,
+    deadline: String
+  }],
+  schedule:[{
+    date: String,
+    events: [String]
+  }],
+  day: [Number]
 },{collection:'users'})
 
 // Creating a table within database with the defined schema
--- backend/src/routes/api/data.js
+++ backend/src/routes/api/data.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import User from '../../models/User';
 const router = Router()
 //http://localhost:4000/api/data/XXX
 
@@ -40,23 +41,39 @@ const schedule = [
 ]
 const day = [3,2,1,4,2,3,2]
 // const schedule
-router.get('/init',(req, res)=>{
+router.get('/init', async(req, res)=>{
     const {username} = req.query
     console.log(`User ${username} comes to init data!`)
+    const {todoList, schedule, day} = await User.findOne({username:username});
     //take something from database 記得error handle
-    res.json({
+    if(day.length === 0){
+      res.json({
+        todoList: todoList,
+        schedule: schedule,
+        day: [1,1,1,1,1,1,1]
+      })
+    }
+    else{
+      res.json({
         todoList: todoList,
         schedule: schedule,
         day: day
-    })
+      })
+    }
 })
 
-router.post('/update',(req, res)=>{
+router.post('/update', async(req, res)=>{
     const {username, todoList, schedule, day} = req.body
     console.log(`User ${username} comes to update data!`)
     console.log(todoList)
     console.log(schedule)
     console.log(day)
+    await User.updateOne({username: username},{
+      todoList: todoList,
+      schedule: schedule,
+      day: day
+    })
+    
     // save to database 記得error handle 也記得提醒小黑handle算不出來的例外
     res.json({message:'success'})
 })
