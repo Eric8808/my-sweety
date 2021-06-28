f658b649e954e0ef4bec5bd964682499829a77eb
Eric8808 <ericchen82957@gmail.com>
2021-06-29 00:34:20 +0800

    deploy on heroku

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,15 +3,28 @@ import express from 'express';
 import routes from './routes';
 import dotenv from 'dotenv'
 import cors from 'cors'
+import http from "http";
+import path from "path";
+import { dirname } from "path";
 dotenv.config()
-
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = process.env.PORT || 80;
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.use('/', routes);
-
+app.use(express.static(path.join(__dirname, "../..", "build")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../..", "build", "index.html"));
+});
+// const httpServer = http.createServer(app);
 mongo.connect();
-
+// httpServer.listen(port, () => {
+//   console.log(`🚀 Server Ready at ${port}! 🚀`);
+//   console.log(__dirname)
+//   console.log(path.join(__dirname, "../..", "build"))
+//   // console.log(`Graphql Port at ${port}${server.subscriptionsPath}`);
+// });
 const server = app.listen(process.env.PORT || 4000, function () {
   console.log('Listening on port ' + server.address().port);
 });
