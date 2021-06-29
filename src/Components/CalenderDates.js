434ecac7ed3fc1a64cdee22098d9c281f5400f63
Eric8808 <ericchen82957@gmail.com>
2021-06-29 15:12:21 +0800

    test auto deploy

--- src/Components/CalenderDates.js
+++ src/Components/CalenderDates.js
@@ -38,7 +38,7 @@ function CalenderDate({week, setWeek}) {
         let dateStyle = {}
         if (week === 0) {
           if (today.getDay() === value) {
-            // dateStyle = {backgroundColor: "#ffab40", borderRadius: '5px'}
+            // 今天會被框起來
             dateStyle = {border: "solid 3px black", borderRadius: '5px'}
           }
         }
